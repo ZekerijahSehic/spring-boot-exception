616689befae9a58ac79706ed16226f089067d024
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-14 14:22:08 +0200

    UserController with e

--- src/main/java/com/zekerijah/exception/controller/UserController.java
+++ src/main/java/com/zekerijah/exception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.zekerijah.exception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -11,4 +13,29 @@ public class UserController {
         int num = 10 / 0;
         return "add";
     }
+
+    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
+    }
+
+    @RequestMapping("/update")
+    public String update() {
+        String name = null;
+        name = name.toLowerCase(); // this should cause null pointer exception
+        return "update";
+    }
+
+    @ExceptionHandler(value={java.lang.NullPointerException.class})
+    public ModelAndView handlerNullPointerException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("nullPointerError");
+        return modelAndView;
+    }
+
+
 }
